199d67ca69da2b7177e0e9570109c237431ca09c
Shay Hoffman <shayhoffman@gmail.com>
2018-04-11 21:44:50 -0500

    added child component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import {Quote} from './Quote';
+import Nav from  './nav.js'
 import selfie from './selfie.JPG';
 import Indie from './Indie.jpg';
 import airplanescreenshot from './airplanescreenshot.png';
@@ -24,28 +25,8 @@ class App extends Component {
     return (
       <div className="body">
 
-        <div className="navigation">
-          <nav className = "navbar navbar-expand-md bg-dark navbar-dark fixed-top">
-            <a className="navbar-brand myName" href="https://www.linkedin.com/in/shay-hoffman-89600a151/" target="_blank"><h3 className="shayName">Shay Hoffman</h3></a>
-
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleFooter">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className = "collapse navbar-collapse" id = "collapsibleFooter">
-              <ul className = "navbar-nav">
-                <li className = "nav-item">
-                  <a className="nav-link" href="#aboutMe">About Me</a>
-                </li>
-                <li>
-                  <a className="nav-link" href="#portfolio">Portfolio</a>
-                </li>
-                <li>
-                  <a className="nav-link" href="#apiQuote">Inspirational Stuff</a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        </div>
+        <Nav id="navigation"/>
+          
 
         <div className="header">
           <div className="row">
---
+++ src/nav.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Nav extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render(){
+    return (
+      <div className="navigation">
+        <nav className = "navbar navbar-expand-md bg-dark navbar-dark fixed-top">
+          <a className="navbar-brand myName" href="https://www.linkedin.com/in/shay-hoffman-89600a151/" target="_blank"><h3 className="shayName">Shay Hoffman</h3></a>
+
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleFooter">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          <div className = "collapse navbar-collapse" id = "collapsibleFooter">
+            <ul className = "navbar-nav">
+              <li className = "nav-item">
+                <a className="nav-link" href="#aboutMe">About Me</a>
+              </li>
+              <li>
+                <a className="nav-link" href="#portfolio">Portfolio</a>
+              </li>
+              <li>
+                <a className="nav-link" href="#apiQuote">Inspirational Stuff</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Nav
